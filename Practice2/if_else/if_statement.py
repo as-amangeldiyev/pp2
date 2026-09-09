8c10e1489f65b5658a0f205acc16fd710e519f5c
Assylzhan <assylzhanamangeldiyev@gmail.com>
2026-09-09 12:51:11 +0500

    Add Practice2 conditional examples

---
+++ Practice2/if_else/if_statement.py
@@ -0,0 +1,6 @@
+temperature = 28
+
+if temperature > 25:
+    print("It is a warm day.")
+
+print("Check complete.")
---
+++ Practice2/if_else/short_hand_if.py
@@ -0,0 +1,6 @@
+number = 8
+
+if number > 5: print("Number is greater than 5")
+
+message = "even" if number % 2 == 0 else "odd"
+print("The number is", message)
